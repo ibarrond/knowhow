61c359814e41ada23680f257c6f413277f183167
Alberto Ibarrondo <ibarrondo.alberto@gmail.com>
2018-11-02 15:27:22 +0100

    Update Timing.py

--- Timing.py
+++ Timing.py
@@ -1,46 +1,11 @@
-from typing import Dict
-from functools import wraps
-from time import process_time
+from contextlib import contextmanager
 
-# PythonDecorators/decorator_with_arguments.py
-class Timing(object):
-    """Timing Decorator Class
+@contextmanager
+def timer(message):
+    "Context manager that reports the time taken by a block of code."
+    print("[*]", message)
+    start = time.time()
+    yield
+    print("[#] finished in {:.3f} seconds".format(time.time() - start))
     
-    A decorator for time measuring of functions.
-    
-    Example:
-      # Create a timing dictionary: keys are strings, values are floats
-      timing_dict: Dict[str, float] = {}
-      
-      # Apply the decorator and run the function
-      @Timing(timing_dict, 'ny_fun1000')
-      def my_fun(1000)
-    """
-
-    def __init__(self,
-                 timings_dict: Dict[str, float]={},
-                 timing_name: str='',
-                 flag_print:  bool=True):
-        self.timings_dict = timings_dict
-        self.timing_name = timing_name
-        self.flag_print = flag_print
-        
-
-    def __call__(self, f):
-        """
- 
-        """
-        @wraps(f)
-        def wrapped_f(*args, **kw):
-            ts = process_time()  # Before
-            f(*args, **kw)       # Execute f
-            te = process_time()  # After
-            
-            # Save time in timings_dict
-            if(not self.timing_name): self.timing_name = f.__name__
-            self.timings_dict[self.timing_name] = (te - ts)*1000
-            
-            # Print timing
-            if(self.flag_print):
-              print('TIMING: %r -> %.6f ms\n' % (f.__name__,(te - ts)*1000))
-        return wrapped_f
+# Use in a `with` statement
